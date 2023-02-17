ceab0a6d35a45838247e9a83ca05136df1e7311c
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2023-02-17 16:54:01 +0700

    Install botsrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,36 +7,27 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posts</title>
 
-    <style>
-        .blog {
-            padding: 5px;
-            border-bottom: 1px solid lightgrey;
-        }
-
-        small {
-            color: grey;
-        }
-
-    </style>
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
-    <h1>Halo ini blog Codepolitan</h1>
 
-    <div>
-        @php
-            $number = 1;
-        @endphp
-
+    <div class="container">
+        <h1>Blog Codepolitan</h1>
         @foreach ($posts as $post)
-            <div class="blog">
-                <h3>{{ $post[0] }} <small>#{{ $number }}</small> </h3>
-                <p>{{ $post[1] }}</p>
+            <div class="card mb-3">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post[0] }} </h5>
+                    <p class="card-text">{{ $post[1] }}</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="" class="btn btn-primary">Selengkapnya</a>
+                </div>
             </div>
-
-            @php
-                $number++;
-            @endphp
         @endforeach
     </div>
 
